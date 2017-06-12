Fixed a bug in weighted pixelwise crossentropy calculation

unet/unet.py
@@ -5,6 +5,49 @@ from keras.layers.advanced_activations import LeakyReLU
 from keras import backend as K
 
 ##############################################
+# GLOBALS
+##############################################
+
+_EPSILON = 10e-6
+
+##############################################
+# UTILITIES
+##############################################
+
+"""
+Filter NaNs from a tensor 't' and replace with value epsilon
+
+# Arguments
+    t: A tensor to filter
+    epsilon: Value to replace NaNs with
+# Returns
+    A tensor of same shape as t with NaN values replaced by epsilon.
+"""
+
+
+def tf_filter_nans(t, epsilon):
+    return K.tf.where(K.tf.is_nan(t), K.tf.ones_like(t) * epsilon, t)
+
+
+"""
+Convert the input `x` to a tensor of type `dtype`.
+
+# Arguments
+    x: An object to be converted (numpy array, list, tensors).
+    dtype: The destination type.
+# Returns
+    A tensor.
+"""
+
+
+def _to_tensor(x, dtype):
+    x = K.tf.convert_to_tensor(x)
+    if x.dtype != dtype:
+        x = K.tf.cast(x, dtype)
+    return x
+
+
+##############################################
 # METRICS
 ##############################################
 
@@ -71,26 +114,6 @@ def mean_iou(num_classes):
 # LOSS/ACTIVATION FUNCTIONS
 ##############################################
 
-_EPSILON = 10e-8
-
-"""
-Convert the input `x` to a tensor of type `dtype`.
-
-# Arguments
-    x: An object to be converted (numpy array, list, tensors).
-    dtype: The destination type.
-# Returns
-    A tensor.
-"""
-
-
-def _to_tensor(x, dtype):
-    x = K.tf.convert_to_tensor(x)
-    if x.dtype != dtype:
-        x = K.tf.cast(x, dtype)
-    return x
-
-
 """
 A per-pixel softmax i.e. each pixel is considered as a sample and the
 class probabilities for each pixel sum to one.
@@ -130,11 +153,6 @@ def pixelwise_crossentropy(y_true, y_pred):
     return K.tf.reduce_sum(K.tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y_pred, labels=labels))
 
 
-# epsilon = _to_tensor(_EPSILON, y_pred.dtype.base_dtype)
-# y_pred = K.tf.clip_by_value(y_pred, epsilon, 1. - epsilon)
-# return - K.tf.reduce_sum(y_true * tf.log(y_pred))
-
-
 """
 Pixel-wise weighted categorical crossentropy between an
 output tensor and a target tensor.
@@ -152,26 +170,20 @@ def weighted_pixelwise_crossentropy(class_weights):
     def loss(y_true, y_pred):
         # Try to increase numerical stability by adding epsilon to predictions
         # to counter very small predictions
-        epsilon = K.tf.constant(value=0.00001, shape=shape)
+        epsilon = _to_tensor(_EPSILON, y_pred.dtype.base_dtype)
         y_pred = y_pred + epsilon
 
         # Calculate cross-entropy loss
         softmax = K.tf.nn.softmax(y_pred)
-        xent = -K.tf.reduce_sum(K.tf.multiply(y_true * K.tf.log(softmax), class_weights))
+        softmax = tf_filter_nans(softmax, _EPSILON)
 
-        # NaN protection
-        xent = K.tf.where(K.tf.is_nan(xent), K.tf.ones_like(xent) * _EPSILON, xent);
+        xent = K.tf.multiply(y_true * K.tf.log(softmax), class_weights)
+        xent = tf_filter_nans(xent, _EPSILON)
+        xent = -K.tf.reduce_sum(xent)
 
         return xent
 
     return loss
-
-
-#        epsilon = _to_tensor(_EPSILON, y_pred.dtype.base_dtype)
-#        y_pred = K.tf.clip_by_value(y_pred, epsilon, 1. - epsilon)
-#        return - K.tf.reduce_sum(K.tf.multiply(y_true * K.tf.log(y_pred), class_weights))
-# labels = K.tf.cast(K.tf.argmax(y_true, axis=-1), K.tf.int32)
-
 
 ##############################################
 # UNET