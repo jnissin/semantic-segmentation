e1b6d01053b0f821e848b70a6c5bd2718b2d7bbb
Joonas Nissinen <joonas.nissinen@futurice.com>
2017-06-14 00:04:52 +0300

    Updated config to continue with batch training

--- unet/slurm/train_batch.sh
+++ unet/slurm/train_batch.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#Request 1 Tesla K80 gpus for 9 hours
+#Request 1 Tesla K80 gpus and 2 CPUs for 9 hours
 #SBATCH --gres=gpu:teslak80:1
 #SBATCH -p gpu
 #SBATCH --mem 32G
+#SBATCH -c 2
 #SBATCH -t 09:00:00
 
 module load anaconda2 CUDA/7.5.18 cudnn/4
