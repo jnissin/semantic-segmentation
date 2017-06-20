cd32320bf7a23e987c2e5bdb256077e617527c5f
jnissin <joonas.nissinen@futurice.com>
2017-06-20 17:52:18 +0300

    Updated slurm scripts

--- src/slurm/train_batch.sh
+++ src/slurm/train_batch.sh
@@ -2,12 +2,13 @@
 
 #Request 1 Tesla K80 gpus and 2 CPUs for 9 hours
 #SBATCH --gres=gpu:teslak80:1
-#SBATCH -p gpu
+#SBATCH -p gpushort
 #SBATCH --mem 32G
 #SBATCH -c 2
-#SBATCH -t 09:00:00
+#SBATCH -t 04:00:00
 
 module load anaconda2 CUDA/7.5.18 cudnn/4
 source activate semantic-segmentation
 
-python /scratch/work/jhnissin/semantic-segmentation/src/train.py /scratch/work/jhnissin/semantic-segmentation/src/config/config-filtered.json
+cd /scratch/work/jhnissin/semantic-segmentation/src
+srun python train.py /scratch/work/jhnissin/semantic-segmentation/src/configs/config-filtered.json
--- src/slurm/train_interactive.sh
+++ src/slurm/train_interactive.sh
@@ -3,4 +3,4 @@
 module load anaconda2 CUDA/7.5.18 cudnn/4
 source activate semantic-segmentation
 
-sinteractive -t 0:30:00 -p gpushort --gres=gpu:teslak80:1 --mem=32G
+sinteractive -t 00:30:00 -p gpushort --gres=gpu:teslak80:1 --mem=32G
