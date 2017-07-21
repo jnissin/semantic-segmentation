Improvements to dataset statistics calculation, verbose mode and zero-division fixes

src/scripts/calculate_dataset_statistics.py
@@ -28,6 +28,8 @@ class DatasetStatistics(object):
         self.mask_statistics = mask_statistics
         self.rseed = rseed
         self.split = split
+        self.per_channel_mean = per_channel_mean
+        self.per_channel_stddev = per_channel_stddev
 
         # Calculate total number of pixels
         self.total_mask_pixels = sum([s.num_pixels for s in self.mask_statistics])
@@ -48,26 +50,38 @@ class DatasetStatistics(object):
                     self.material_occurrences[i] += 1
                     self.material_occurrence_files[i].append(s.image_name)
 
-        # Calculate median frequency balancing weights
-        # freq(c) is the number of pixels of class c divided
-        # by the total number of pixels in images where c is present.
-        # Median freq is the median of these frequencies.
+        # Calculate median frequency balancing weights. The frequency of a material freq(c)
+        # is the number of pixels of class c divided by the total number of pixels in images
+        # where c is present. Median freq is the median of these frequencies. If a material is not
+        # present in the training set it will have a zero weight.
         total_pixels_per_material = [0] * len(self.material_pixels)
 
         for s in self.mask_statistics:
             for i in range(0, len(s.material_pixels)):
-                if s.material_pixels[i] != 0:
+                if s.material_pixels[i] > 0:
                     total_pixels_per_material[i] += s.num_pixels
 
         np_material_pixels = np.array(self.material_pixels)
         np_total_pixels_per_material = np.array(total_pixels_per_material)
-        np_material_frequencies = np_material_pixels / np_total_pixels_per_material
+
+        # Avoid NaNs because of zero material frequencies
+        np_material_frequencies = np.zeros_like(np_material_pixels)
+
+        for i in range(0, np_total_pixels_per_material.shape[0]):
+            if np_total_pixels_per_material[i] != 0:
+                np_material_frequencies[i] = np_material_pixels[i] / np_total_pixels_per_material[i]
+
+        # Take the median frequency
         np_median_material_frequency = np.median(np_material_frequencies)
-        np_median_material_frequency_weights = np_median_material_frequency / np_material_frequencies
+
+        # Avoid NaNs because of zero material frequencies
+        np_median_material_frequency_weights = np.zeros_like(np_material_frequencies)
+
+        for i in range(0, np_material_frequencies.shape[0]):
+            if np_material_frequencies[i] != 0:
+                np_median_material_frequency_weights[i] = np_median_material_frequency / np_material_frequencies[i]
 
         self.median_frequency_balancing_weights = np_median_material_frequency_weights.tolist()
-        self.per_channel_mean = per_channel_mean
-        self.per_channel_stddev = per_channel_stddev
 
 
 class MaskStatistics(object):
@@ -90,7 +104,7 @@ def calculate_mask_statistics(mask_file_path, materials):
     num_pixels = mask_img_array.shape[0] * mask_img_array.shape[1]
     material_pixels = []
 
-    for i in range(0, expanded_mask.shape[2]):
+    for i in range(0, expanded_mask.shape[-1]):
         material_pixels.append(float(np.sum(expanded_mask[:, :, i])))
 
     img_stats = MaskStatistics(image_name, num_pixels, material_pixels)
@@ -167,14 +181,14 @@ def main():
     print 'Starting per-channel mean calculation for {} photos with {} jobs'.format(len(photos), n_jobs)
 
     start_time = time.time()
-    per_channel_mean = dataset_utils.calculate_per_channel_mean(photos, 3)
+    per_channel_mean = dataset_utils.calculate_per_channel_mean(photos, 3, verbose=True)
 
     print 'Per-channel mean calculation for {} files finished in {} seconds'\
         .format(len(photos), time.time()-start_time)
 
     print 'Starting per-channel stddev calculation for {} photos with {} jobs'.format(len(photos), n_jobs)
 
-    per_channel_stddev = dataset_utils.calculate_per_channel_stddev(photos, per_channel_mean, 3)
+    per_channel_stddev = dataset_utils.calculate_per_channel_stddev(photos, per_channel_mean, 3, verbose=True)
 
     print 'Per-channel stddev calculation for {} files finished in {} seconds'\
         .format(len(photos), time.time()-start_time)

src/utils/dataset_utils.py
@@ -170,8 +170,11 @@ def get_files(path, ignore_hidden_files=True):
     return files
 
 
-def calculate_per_channel_mean(file_paths, num_channels):
-    # type: (list[str], int) -> np.array[np.float32]
+_per_channel_mean_processed = 0
+
+
+def calculate_per_channel_mean(file_paths, num_channels, verbose=False):
+    # type: (list[str], int, bool) -> np.array[np.float32]
 
     """
     Calculates the per-channel mean from all the images in the given
@@ -189,7 +192,7 @@ def calculate_per_channel_mean(file_paths, num_channels):
 
     data = Parallel(n_jobs=n_jobs, backend='threading')(
         delayed(_calculate_per_channel_mean)(
-            fp, num_channels) for fp in file_paths)
+            fp, num_channels, verbose) for fp in file_paths)
 
     # Calculate the final value
     sums = np.sum(np.vstack(data), axis=0)
@@ -199,10 +202,13 @@ def calculate_per_channel_mean(file_paths, num_channels):
     per_channel_mean = color_tot / px_tot
     print 'Per-channel mean calculation complete: {}'.format(per_channel_mean)
 
+    global _per_channel_mean_processed
+    _per_channel_mean_processed = 0
+
     return per_channel_mean
 
 
-def _calculate_per_channel_mean(path, num_channels):
+def _calculate_per_channel_mean(path, num_channels, verbose):
     img = load_n_channel_image(path, num_channels)
     img_array = img_to_array(img)
 
@@ -219,11 +225,21 @@ def _calculate_per_channel_mean(path, num_channels):
     # In the final index store the number of pixels
     tot[num_channels] = img_array.shape[0] * img_array.shape[1]
 
+    if verbose:
+        global _per_channel_mean_processed
+        _per_channel_mean_processed += 1
+
+        if _per_channel_mean_processed%1000 == 0:
+            print 'Per-channel mean: processed {} images'.format(_per_channel_mean_processed)
+
     return tot
 
 
-def calculate_per_channel_stddev(file_paths, per_channel_mean, num_channels):
-    # type: (list[str], np.array[np.float32], int) -> np.array[np.float32]
+_per_channel_stddev_processed = 0
+
+
+def calculate_per_channel_stddev(file_paths, per_channel_mean, num_channels, verbose=False):
+    # type: (list[str], np.array[np.float32], int, bool) -> np.array[np.float32]
 
     """
     Calculates the per-channel standard deviation from all the images in the given
@@ -231,7 +247,9 @@ def calculate_per_channel_stddev(file_paths, per_channel_mean, num_channels):
 
     # Arguments
         :param file_paths: paths to the files to be used in the calculation
+        :param per_channel_mean: per channel mean for the data set in range [-1,1]
         :param num_channels: number of channels in the image 1,3 or 4
+        :param verbose: print information about the progress of the calculation
     # Returns
         :return: numpy array with the channel means in range [-1, 1]
     """
@@ -241,7 +259,7 @@ def calculate_per_channel_stddev(file_paths, per_channel_mean, num_channels):
 
     data = Parallel(n_jobs=n_jobs, backend='threading')(
         delayed(_calculate_per_channel_stddev)(
-            fp, per_channel_mean, num_channels) for fp in file_paths)
+            fp, per_channel_mean, num_channels, verbose) for fp in file_paths)
 
     # Calculate the final value
     sums = np.sum(np.vstack(data), axis=0)
@@ -256,10 +274,13 @@ def calculate_per_channel_stddev(file_paths, per_channel_mean, num_channels):
     per_channel_stddev = np.sqrt(per_channel_var)
     print 'Per-channel stddev calculation complete: {}'.format(per_channel_stddev)
 
+    global _per_channel_stddev_processed
+    _per_channel_stddev_processed = 0
+
     return per_channel_stddev
 
 
-def _calculate_per_channel_stddev(path, per_channel_mean, num_channels):
+def _calculate_per_channel_stddev(path, per_channel_mean, num_channels, verbose):
     var_tot = np.array([0.0] * (num_channels + 1))
 
     # Load the image as numpy array
@@ -275,6 +296,13 @@ def _calculate_per_channel_stddev(path, per_channel_mean, num_channels):
 
     # Accumulate the number of total pixels
     var_tot[-1] = img_array.shape[0] * img_array.shape[1]
+
+    if verbose:
+        global _per_channel_stddev_processed
+        _per_channel_stddev_processed += 1
+
+        if _per_channel_stddev_processed%1000 == 0:
+            print 'Per-channel stddev: processed {} images'.format(_per_channel_stddev_processed)
 
     return var_tot
 