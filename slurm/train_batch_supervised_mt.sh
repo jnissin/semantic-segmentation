37fc699af044bed8c6769d258acc0456cdae03c4
jnissin <joonas.nissinen@futurice.com>
2017-07-24 19:38:42 +0300

    Fixes to dependencies and slurm run scripts

--- slurm/train_batch_supervised_mt.sh
+++ slurm/train_batch_supervised_mt.sh
@@ -2,7 +2,7 @@
 
 #Request 1 Tesla K80 gpu(s) and 4 CPU(s) for 1 day(s)
 #SBATCH --gres=gpu:teslak80:1
-#SBATCH -p gpushort
+#SBATCH -p gpu
 #SBATCH --mem 32G
 #SBATCH -c 4
 #SBATCH -t 01:00:00:00
@@ -13,4 +13,4 @@ module load anaconda2
 source activate semantic-segmentation
 module load CUDA/8.0.61 cudnn/5.1-CUDA-7.5
 
-srun python -m src.train --model enet-naive-upsampling --mfolder enet-naive-upsampling-supervised-mt --trainer segmentation --config ./config/config-segmentation-supervised-mt.json
+srun python -m src.train --model enet-naive-upsampling --mfolder enet-naive-upsampling-supervised-mt --trainer segmentation --config ./configs/config-segmentation-supervised-mt.json --wdir /scratch/work/jhnissin/semantic-segmentation/
