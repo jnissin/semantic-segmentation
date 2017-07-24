88bd3374e133d0efecd6184e80c09564566bde28
jnissin <joonas.nissinen@futurice.com>
2017-07-24 19:07:56 +0300

    Added configurations and slurm bash scripts for training different versions of ENet

---
+++ slurm/train_batch_semisupervised_mt.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Request 1 Tesla K80 gpu(s) and 4 CPU(s) for 1 day(s)
+#SBATCH --gres=gpu:teslak80:1
+#SBATCH -p gpushort
+#SBATCH --mem 32G
+#SBATCH -c 4
+#SBATCH -t 01:00:00:00
+#SBATCH -D /scratch/work/jhnissin/semantic-segmentation/
+
+module purge
+module load anaconda2
+source activate semantic-segmentation
+module load CUDA/8.0.61 cudnn/5.1-CUDA-7.5
+
+srun python -m src.train --model enet-naive-upsampling --mfolder enet-naive-upsampling-semisupervised-mt --trainer semisupervised-segmentation --config ./config/config-segmentation-semisupervised-mt.json
--- slurm/train_interactive.sh
+++ slurm/train_interactive.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+sinteractive -t 00:30:00 -p gpushort --gres=gpu:teslak80:1 --mem=32G
+
 module purge
 module load anaconda2
 source activate semantic-segmentation
 module load CUDA/8.0.61 cudnn/5.1-CUDA-7.5
 
-sinteractive -t 00:30:00 -p gpushort --gres=gpu:teslak80:1 --mem=32G
+cd /scratch/work/jhnissin/semantic-segmentation/
