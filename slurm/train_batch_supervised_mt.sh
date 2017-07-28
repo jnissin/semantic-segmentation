c1295b4fcd3fb04f582b17586250d0284f522061
jnissin <joonas.nissinen@futurice.com>
2017-07-28 20:41:28 +0300

    Changed slurm requests to two days

--- slurm/train_batch_supervised_mt.sh
+++ slurm/train_batch_supervised_mt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#Request 1 Tesla K80 gpu(s) and 4 CPU(s) for 1 day(s)
+#Request 1 Tesla K80 gpu(s) and 4 CPU(s) for 2 day(s)
 #SBATCH --gres=gpu:teslak80:1
 #SBATCH -p gpu
 #SBATCH --mem 32G
 #SBATCH -c 4
-#SBATCH -t 01-12:00:00
+#SBATCH -t 02-00:00:00
 #SBATCH -D /scratch/work/jhnissin/semantic-segmentation/
 
 module purge
