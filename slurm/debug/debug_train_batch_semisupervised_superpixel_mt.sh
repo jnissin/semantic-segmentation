8a69906a04b682ea2e9383c5c1655525fe283531
jnissin <joonas.nissinen@futurice.com>
2017-08-03 14:29:07 +0300

    Debug scripts for training on a small dataset

---
+++ slurm/debug/debug_train_batch_semisupervised_superpixel_mt.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Request 1 Tesla K80 gpu(s) and 4 CPU(s) for 2 day(s)
+#SBATCH --gres=gpushort:teslak80:1
+#SBATCH -p gpu
+#SBATCH --mem 16G
+#SBATCH -c 4
+#SBATCH -t 04:00:00
+#SBATCH -D /scratch/work/jhnissin/semantic-segmentation/
+
+module purge
+module load anaconda2
+source activate semantic-segmentation
+module load CUDA/8.0.61 cudnn/5.1-CUDA-7.5
+
+srun python -m src.train --model yolonet --mfolder yolonet-semisupervised-superpixel-mt-debug --trainer semisupervised-segmentation --config ./configs/debug/config-segmentation-semisupervised-superpixel-mt-debug.json --wdir /scratch/work/jhnissin/semantic-segmentation/ --maxjobs 4
