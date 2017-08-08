a73881c4b4212843ed1a3563e1218f2961a1de54
jnissin <joonas.nissinen@futurice.com>
2017-08-08 11:21:24 +0300

    Switched debug runs to use yolonet

--- slurm/debug/debug_train_batch_semisupervised_superpixel_mt.sh
+++ slurm/debug/debug_train_batch_semisupervised_superpixel_mt.sh
@@ -13,4 +13,4 @@ module load anaconda2
 source activate semantic-segmentation
 module load CUDA/8.0.61 cudnn/5.1-CUDA-7.5
 
-srun python -m src.train --model enet-naive-upsampling --mfolder enet-naive-upsampling-semisupervised-superpixel-mt-debug --trainer semisupervised-segmentation --config ./configs/debug/config-segmentation-semisupervised-superpixel-mt-debug.json --wdir /scratch/work/jhnissin/semantic-segmentation/ --maxjobs 4
+srun python -m src.train --model yolonet --mfolder yolonet-semisupervised-superpixel-mt-debug --trainer semisupervised-segmentation --config ./configs/debug/config-segmentation-semisupervised-superpixel-mt-debug.json --wdir /scratch/work/jhnissin/semantic-segmentation/ --maxjobs 4
