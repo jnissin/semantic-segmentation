9663ce760b5baea9a22d8b62d697d94e5475622e
jnissin <joonas.nissinen@futurice.com>
2017-09-11 23:29:54 +0300

    Modified for Adaptive Sampling run

--- slurm/train_batch_supervised_mt.sh
+++ slurm/train_batch_supervised_mt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Request 1 Tesla K80 or P100. The 'hsw' constraint guarantees K80 or P100 because they are the only GPU nodes with hsw processors
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:teslap100:1
 #SBATCH --constraint=hsw
 #SBATCH -p gpu
 #SBATCH --mem 16G
@@ -14,4 +14,4 @@ module load anaconda2
 source activate semantic-segmentation
 module load CUDA/8.0.61 cudnn/5.1-CUDA-7.5
 
-srun python -m src.train --model enet-naive-upsampling --mfolder enet-naive-upsampling/supervised-mt-pre-selective --trainer segmentation_supervised_mean_teacher --config ./configs/config-segmentation.json --wdir /scratch/work/jhnissin/semantic-segmentation/ --maxjobs 4
+srun python -m src.train --model enet-naive-upsampling --mfolder enet-naive-upsampling/supervised-mt-pre-selective-as --trainer segmentation_supervised_mean_teacher --config ./configs/config-segmentation.json --wdir /scratch/work/jhnissin/semantic-segmentation/ --maxjobs 4
