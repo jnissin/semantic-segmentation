d646a364b5daaac1b1d00e3a9974f4b8787d418a
jnissin <joonas.nissinen@futurice.com>
2017-10-19 20:34:50 +0300

    Hacked data utils to catch the whole exception which crashes the run

--- src/keras_extensions/utils/data_utils.py
+++ src/keras_extensions/utils/data_utils.py
@@ -267,14 +267,14 @@ class OrderedEnqueuer(SequenceEnqueuer):
             Generator yielding tuples (inputs, targets)
                 or (inputs, targets, sample_weights)
         """
-        try:
-            while self.is_running():
-                inputs = self.queue.get(block=True).get()
-                if inputs is not None:
-                    yield inputs
-        except Exception as e:
-            self.stop()
-            raise StopIteration(e)
+        #try:
+        while self.is_running():
+            inputs = self.queue.get(block=True).get()
+            if inputs is not None:
+                yield inputs
+        #except Exception as e:
+        #    self.stop()
+        #    raise StopIteration(e)
 
     def _send_sequence(self):
         """Send current Sequence to all workers."""
