5d389977e9dbcb8cede2c3137502f8ea9af650e8
jnissin <joonas.nissinen@futurice.com>
2017-12-04 13:56:51 +0200

    Configurations for semi-supervised fine tuning runs

--- slurm/segmentation/train_batch_semi_supervised_mt_superpixel.sh
+++ slurm/segmentation/train_batch_semi_supervised_mt_superpixel.sh
@@ -3,7 +3,7 @@
 # Request 1 Tesla K80 or P100. The 'hsw' constraint guarantees K80 or P100 because they are the only GPU nodes with hsw processors
 #SBATCH --gres=gpu:teslap100:1
 #SBATCH -p gpu
-#SBATCH --mem 42G
+#SBATCH --mem 38G
 #SBATCH -c 8
 #SBATCH -t 02-00:00:00
 #SBATCH -D /scratch/work/jhnissin/semantic-segmentation/
@@ -14,4 +14,4 @@ module load teflon
 source activate ss-new
 module load CUDA/9.0.176 cuDNN/7-CUDA-9.0.176
 
-srun python -m src.train --model enet-naive-upsampling --mfolder enet-naive-upsampling/semi-supervised-mt-superpixel --trainer segmentation_semi_supervised_mean_teacher_superpixel --config ./configs/segmentation/config-segmentation-semi-supervised-mt-superpixel.json --wdir /scratch/work/jhnissin/semantic-segmentation/ --maxjobs 6
+srun python -m src.train --model enet-naive-upsampling --mfolder enet-naive-upsampling/semi-supervised-mt-superpixel --trainer segmentation_semi_supervised_mean_teacher_superpixel --config ./configs/segmentation/config-segmentation-semi-supervised-mt.json --wdir /scratch/work/jhnissin/semantic-segmentation/ --maxjobs 6
