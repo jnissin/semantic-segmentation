Fixes to max unpooling and L2 softmax for ENet enhanced

src/layers/pooling.py
@@ -75,7 +75,7 @@ class MaxUnpooling2D(Layer):
         Add Theano backend
         """
         pool = inputs[0]
-        ind = inputs[1]
+        ind = K.tf.cast(inputs[1], dtype=K.tf.int64)
 
         with K.tf.variable_scope(self.name):
             input_shape = K.tf.shape(pool, out_type='int64')
@@ -84,15 +84,15 @@ class MaxUnpooling2D(Layer):
             flat_input_size = K.tf.reduce_prod(input_shape)
             flat_output_shape = [output_shape[0], output_shape[1] * output_shape[2] * output_shape[3]]
 
-            pool_ = K.tf.reshape(pool, [flat_input_size])
-            batch_range = K.tf.reshape(K.tf.range(tf.cast(output_shape[0], K.tf.int64), dtype=ind.dtype), shape=[input_shape[0], 1, 1, 1])
+            pool_ = K.tf.reshape(pool, shape=K.tf.stack([flat_input_size]))
+            batch_range = K.tf.reshape(K.tf.range(K.tf.cast(output_shape[0], dtype=K.tf.int64), dtype=ind.dtype), shape=K.tf.stack([input_shape[0], 1, 1, 1]))
             b = K.tf.ones_like(ind) * batch_range
-            b1 = K.tf.reshape(b, [flat_input_size, 1])
-            ind_ = K.tf.reshape(ind, [flat_input_size, 1])
+            b1 = K.tf.reshape(b, shape=K.tf.stack([flat_input_size, 1]))
+            ind_ = K.tf.reshape(ind, shape=K.tf.stack([flat_input_size, 1]))
             ind_ = K.tf.concat([b1, ind_], 1)
 
             ret = K.tf.scatter_nd(ind_, pool_, shape=K.tf.cast(flat_output_shape, K.tf.int64))
-            ret = K.tf.reshape(ret, output_shape)
+            ret = K.tf.reshape(ret, shape=K.tf.stack(output_shape))
 
             set_input_shape = pool.get_shape()
             set_output_shape = [set_input_shape[0],

src/models.py
@@ -1358,7 +1358,7 @@ class ENetMaxUnpooling(ModelBase):
                       kernel_size=(nb_row, nb_col),
                       padding='same',
                       strides=strides,
-                      name='input_block_conv2d')(inp)
+                      name='initial_block_conv2d')(inp)
 
         max_pool, indices = MaxPoolingWithArgmax2D(name='initial_block_pool2d')(inp)
         merged = concatenate([conv, max_pool], axis=3, name='initial_block_concat')
@@ -1766,9 +1766,22 @@ class ENetNaiveUpsamplingEnhanced(ModelBase):
 
         # Unlike in regular ENet we will replace Conv2DTranspose with a Nearest-Neighbour
         # upsampling on each layer plus a Conv2D. This is called a NN-Resize convolution
-        enet = UpSampling2D(size=(2, 2), name='logits_upsample2d')(enet)
+        enet = UpSampling2D(size=(2, 2), name='final_nn_rs_conv_upsample2d')(enet)
         enet = Conv2D(filters=nc,
-                      kernel_size=(2, 2),
+                      kernel_size=(3, 3),
+                      kernel_initializer='he_normal',
+                      bias_initializer='zeros',
+                      padding='same',
+                      name='final_nn_rs_conv2d')(enet)
+
+        # L2 normalization
+        alpha = 40.0
+        enet = K.tf.div(enet, K.tf.norm(enet, axis=-1, ord='euclidean'))
+        enet = K.tf.multiply(enet, alpha)
+
+        # Final classification layer
+        enet = Conv2D(filters=nc,
+                      kernel_size=(1, 1),
                       kernel_initializer='he_normal',
                       bias_initializer='zeros',
                       padding='same',