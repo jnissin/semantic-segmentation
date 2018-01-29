6a3915d50f61f6e34017ed793cc5488e8d804e6d
Joonas Nissinen <joonas.nissinen@aalto.fi>
2018-01-29 14:07:55 +0200

    New configurations and slurm scripts for classification runs

---
+++ slurm/classification/enet-enhanced/train_batch_supervised_mt_enhanced.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Request 1 Tesla K80 or P100. The 'hsw' constraint guarantees K80 or P100 because they are the only GPU nodes with hsw processors
+#SBATCH --gres=gpu:teslap100:1
+#SBATCH -p gpu
+#SBATCH --mem 26G
+#SBATCH -c 7
+#SBATCH -t 02-00:00:00
+#SBATCH -D /scratch/work/jhnissin/semantic-segmentation/
+
+module purge
+module load anaconda2
+module load teflon
+source activate ss-new
+module load CUDA/9.0.176 cuDNN/7-CUDA-9.0.176
+
+srun python -m src.train --model enet-naive-upsampling-enhanced-encoder-only --mfolder enet-naive-upsampling-enhanced-encoder-only/supervised-mt --trainer classification_supervised_mean_teacher --config ./configs/classification/config-classification.json --wdir /scratch/work/jhnissin/semantic-segmentation/ --maxjobs 6
--- slurm/classification/enet/train_batch_supervised_mt.sh
+++ slurm/classification/enet/train_batch_supervised_mt.sh
@@ -3,9 +3,9 @@
 # Request 1 Tesla K80 or P100. The 'hsw' constraint guarantees K80 or P100 because they are the only GPU nodes with hsw processors
 #SBATCH --gres=gpu:teslap100:1
 #SBATCH -p gpu
-#SBATCH --mem 32G
-#SBATCH -c 8
-#SBATCH -t 03-00:00:00
+#SBATCH --mem 26G
+#SBATCH -c 7
+#SBATCH -t 02-00:00:00
 #SBATCH -D /scratch/work/jhnissin/semantic-segmentation/
 
 module purge
@@ -14,4 +14,4 @@ module load teflon
 source activate ss-new
 module load CUDA/9.0.176 cuDNN/7-CUDA-9.0.176
 
-srun python -m src.train --model enet-naive-upsampling-encoder-only --mfolder enet-naive-upsampling-encoder-only/supervised-mt --trainer classification_supervised_mean_teacher --config ./configs/config-classification.json --wdir /scratch/work/jhnissin/semantic-segmentation/ --maxjobs 6
+srun python -m src.train --model enet-naive-upsampling-encoder-only --mfolder enet-naive-upsampling-encoder-only/supervised-mt --trainer classification_supervised_mean_teacher --config ./configs/classification/config-classification.json --wdir /scratch/work/jhnissin/semantic-segmentation/ --maxjobs 6
