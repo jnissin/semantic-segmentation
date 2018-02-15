Fixed border connectivity in superpixel mask generation. Removed unnecessary code from weight calculation.

src/utils/dataset_utils.py
@@ -553,35 +553,35 @@ class SegmentationDataSetInformation(object):
 
             num_material_classes = len(self.training_set.material_samples)
             class_probabilities = np.zeros(num_material_classes, dtype=np.float32)
-            crop_area = crop_shape[0] * crop_shape[1]
+            crop_pixels = crop_shape[0] * crop_shape[1]
 
             for material_class_id in range(0, num_material_classes):
                 if material_class_id in ignore_classes:
+                    class_probabilities[material_class_id] = 0.0
                     continue
 
                 p_material_class = 0.0
 
+                # Calculate average percentage of material pixels in a crop
+                # for the material class
                 for material_sample in self.training_set.material_samples[material_class_id]:
-                    # Calculate bbox height and width in pixels
+                    # Calculate percentage of material pixels within the material sample
+                    # bounding box area
                     bbox_height = max(material_sample.yx_max[0] - material_sample.yx_min[0], 1)
                     bbox_width = max(material_sample.yx_max[1] - material_sample.yx_min[1], 1)
-                    bbox_area = bbox_height * bbox_width
+                    bbox_pixels = bbox_height * bbox_width
 
-                    # Calculate proportion of material sample pixels of the bbox area
-                    p_material_pixels = min(float(material_sample.num_material_pixels) / float(bbox_area), 1.0)
-
-                    # Calculate limit bbox dimensions to crop dimensions
-                    bbox_height = min(crop_shape[0], bbox_height)
-                    bbox_width = min(crop_shape[1], bbox_width)
-                    bbox_area = bbox_height * bbox_width
+                    p_material_pixels = min(float(material_sample.num_material_pixels) / float(bbox_pixels), 1.0)
 
                     # Calculate proportion of bbox pixels of the crop pixels
-                    p_bbox_pixels_in_crop = min(float(bbox_area) / float(crop_area), 1.0)
+                    p_bbox_pixels_in_crop = min(float(bbox_pixels) / float(crop_pixels), 1.0)
 
-                    # Calculate proportion of material pixels of the crop pixels
+                    # Calculate proportion (~probability) of material pixels of the crop pixels
                     p = p_material_pixels * p_bbox_pixels_in_crop
                     p_material_class += p
 
+                # Calculate the average proportion (~probability) of all the material samples
+                # within the material class. This is the material class probability.
                 p_material_class = p_material_class / len(self.training_set.material_samples[material_class_id])
                 class_probabilities[material_class_id] = p_material_class
 

src/utils/image_utils.py
@@ -1500,7 +1500,7 @@ def np_adaptive_histogram_equalization(np_img, nbins=256):
 #######################################################
 
 
-def np_get_slic_segmentation(np_img, n_segments, sigma=0.8, compactness=2, max_iter=20, normalize_img=False, borders_only=False):
+def np_get_slic_segmentation(np_img, n_segments, sigma=0.8, compactness=2, max_iter=20, normalize_img=False, borders_only=False, border_connectivity=1):
     # type: (np.ndarray, int, int, float) -> np.ndarray
 
     """
@@ -1527,12 +1527,12 @@ def np_get_slic_segmentation(np_img, n_segments, sigma=0.8, compactness=2, max_i
         segments = slic(np_img, n_segments=n_segments, sigma=sigma, compactness=compactness, max_iter=max_iter)
 
     if borders_only:
-        segments = np.invert(find_boundaries(segments, mode='thick')).astype(np.int32)
+        segments = np.invert(find_boundaries(segments, mode='thick', connectivity=border_connectivity)).astype(np.int32)
 
     return segments
 
 
-def np_get_felzenszwalb_segmentation(np_img, scale=1, sigma=0.8, min_size=20, multichannel=True, normalize_img=False, borders_only=False):
+def np_get_felzenszwalb_segmentation(np_img, scale=1, sigma=0.8, min_size=20, multichannel=True, normalize_img=False, borders_only=False, border_connectivity=1):
     # type: (np.ndarray, float, float, int, bool) -> np.ndarray
 
     from skimage.segmentation import felzenszwalb, find_boundaries
@@ -1544,12 +1544,12 @@ def np_get_felzenszwalb_segmentation(np_img, scale=1, sigma=0.8, min_size=20, mu
         segments = felzenszwalb(image=np_img, scale=scale, sigma=sigma, min_size=min_size, multichannel=multichannel)
 
     if borders_only:
-        segments = np.invert(find_boundaries(segments, mode='thick')).astype(np.int32)
+        segments = np.invert(find_boundaries(segments, mode='thick', connectivity=border_connectivity)).astype(np.int32)
 
     return segments
 
 
-def np_get_watershed_segmentation(np_img, markers, compactness=0.001, normalize_img=False, borders_only=False):
+def np_get_watershed_segmentation(np_img, markers, compactness=0.001, normalize_img=False, borders_only=False, border_connectivity=1):
     # type: (np.ndarray, int, float) -> np.ndarray
 
     from skimage.segmentation import watershed, find_boundaries
@@ -1565,12 +1565,12 @@ def np_get_watershed_segmentation(np_img, markers, compactness=0.001, normalize_
     segments = watershed(gradient, markers=markers, compactness=compactness)
 
     if borders_only:
-        segments = np.invert(find_boundaries(segments, mode='thick')).astype(np.int32)
+        segments = np.invert(find_boundaries(segments, mode='thick', connectivity=border_connectivity)).astype(np.int32)
 
     return segments
 
 
-def np_get_quickshift_segmentation(np_img, kernel_size=3, max_dist=6, sigma=0, ratio=0.5, normalize_img=False, borders_only=False):
+def np_get_quickshift_segmentation(np_img, kernel_size=3, max_dist=6, sigma=0, ratio=0.5, normalize_img=False, borders_only=False, border_connectivity=1):
     # type: (np.ndarray, float, float, float, float) -> np.ndarray
 
     from skimage.segmentation import quickshift, find_boundaries
@@ -1582,6 +1582,6 @@ def np_get_quickshift_segmentation(np_img, kernel_size=3, max_dist=6, sigma=0, r
         segments = quickshift(np_img, kernel_size=kernel_size, max_dist=max_dist, sigma=sigma, ratio=ratio)
 
     if borders_only:
-        segments = np.invert(find_boundaries(segments, mode='thick')).astype(np.int32)
+        segments = np.invert(find_boundaries(segments, mode='thick', connectivity=border_connectivity)).astype(np.int32)
 
     return segments