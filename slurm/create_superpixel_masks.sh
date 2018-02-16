3be77213676f978f76838db8dec9a17c8ac8c91b
Joonas Nissinen <joonas.nissinen@aalto.fi>
2018-02-16 16:56:40 +0200

    Added slurm script for superpixel mask generation to version control

--- slurm/create_superpixel_masks.sh
+++ slurm/create_superpixel_masks.sh
@@ -1,1 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+#SBATCH --mem 18G
+#SBATCH -c 20
+#SBATCH -t 01-00:00:00
+#SBATCH -D /scratch/work/jhnissin/semantic-segmentation/
+
+module purge
+module load anaconda2
+module load teflon
+source activate ss-new
+
+cd /scratch/work/jhnissin/semantic-segmentation/
+
+python -m src.scripts.create_superpixel_masks --photos data/final/unlabeled/ --output data/final/unlabeled_fzw_masks/ --function felzenswalb --jobs 20 --verbose true --dtype float --equalize true --iexisting true --bonly true --bconnectivity 2
